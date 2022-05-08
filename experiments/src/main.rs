6124507150cf65c4cbcee33ddef0494a19b12e88
hhourani27 <61935766+hhourani27@users.noreply.github.com>
2022-05-08 20:29:51 +0200

    Create project for tests

---
+++ experiments/src/main.rs
@@ -0,0 +1,24 @@
+use std::thread;
+
+fn f1(count: usize) {
+    for i in 0..count { /*do something*/ }
+}
+
+fn f2(count: usize) {
+    for i in 0..count { /*do something*/ }
+}
+
+fn run(ops: &Vec<&'static (dyn Fn(usize) + Sync)>) {
+    for i in 0..ops.len() {
+        let func = ops[i];
+        thread::spawn(move || func(1000));
+    }
+}
+
+fn main() {
+    let ops = vec![f1, f1];
+
+    let ops: Vec<&'static (dyn Fn(usize) + Sync)> = vec![&f1, &f2];
+
+    run(&ops);
+}
